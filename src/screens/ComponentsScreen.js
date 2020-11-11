fc6b936e693d510f2b81a44a422edad717bda138
Katia <katia.punter@storyshareplatform.com>
2020-11-11 19:38:19 +0000

    Proforma of the first extra component

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const ComponentScreen = () => {
+    return <Text>Lalalala!</Text>;
+};
+
+export default ComponentScreen;
