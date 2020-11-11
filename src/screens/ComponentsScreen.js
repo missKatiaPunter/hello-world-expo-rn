2138b95cf0329ba9c0babdb1caa1f9abdc0495f4
Katia <katia.punter@storyshareplatform.com>
2020-11-11 21:30:18 +0000

    Attempts a style on a first custom component

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const ComponentScreen = () => {
-    return <Text>Lalalala!</Text>;
+    return <Text style={ styles.text }>Lalalala!</Text>;
 };
 
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 30,
+        color: '#3b2975',
+    }
+});
 export default ComponentScreen;
