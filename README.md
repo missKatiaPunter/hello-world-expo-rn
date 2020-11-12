499ede01fb6571f5078c33c8df8d21a124e6ca0b
Katia <katia.punter@storyshareplatform.com>
2020-11-12 13:08:40 +0000

    Adds another note

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # My first expo app and notes
 
 `navigator` in `App.js` is a tool from the library [react-navigation](https://reactnavigation.org/).
+
+You can use inine css as so:
+
+```js
+const ComponentsScreen = () => {
+    return <Text style={{fontSize: 70}}>Lalalala! It works</Text>;
+};
+```
+
+> Inline css does not error if there was a typo. It gives a warning.
--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
+// const ComponentsScreen = () => {
+//     return <Text style={ styles.text }>Lalalala! It works</Text>;
+// };
+
+// You do not have to use StyleSheet:
 const ComponentsScreen = () => {
-    return <Text style={ styles.text }>Lalalala! It works</Text>;
+    return <Text style={{fontSize: 70}}>Lalalala! It works</Text>;
 };
 
 const styles = StyleSheet.create({
