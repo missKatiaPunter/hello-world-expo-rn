abc1e9ed6a8aa578f2d69fb376cb0fbe691ee1d2
Katia <katia.punter@storyshareplatform.com>
2020-11-13 13:05:26 +0000

    Makes list horizontal

--- src/examples/FlatListComponent.js
+++ src/examples/FlatListComponent.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import { Text, View, StyleSheet, FlatList } from 'react-native';
 
-
-
 const FlatListComponent = () => {
 
     const makeArrayOfNumberedFriends = (num) => {
-        return [...Array(num).keys()].map(el => {
+        return [...Array(num).keys()].map((el,i) => {
             return {
-                name: `Friend#${el+1}`
+                name: `MyFriend#${el+1}`,
+                key: `${i}`
             }
         });
     } 
 
-    const friends = makeArrayOfNumberedFriends(5);
+    const friends = makeArrayOfNumberedFriends(7);
 
     return (
         <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={ false }
             data = { friends }
+            keyExtractor={ item => item.key }
             renderItem = {({ item }) => {
-                return <Text>{ item.name }</Text>;
+                return <Text style={ styles.textStyle }>{ item.name }</Text>;
             }}
+
         />
     );
 
 }
 
 const styles = StyleSheet.create({
-
+    textStyle: {
+        marginVertical: 50
+    }
 });
 
 export default FlatListComponent;
