e8f4708c03b59f50d079ea422ac70df859b4ac80
Katia <katia.punter@storyshareplatform.com>
2020-11-14 09:21:42 +0000

    Adds View and Button to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello World!</Text>;
+    return(
+        <View>
+            <Text style={styles.text}>Hello World!</Text>
+            <Button />
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
-  }
+    text: {
+        fontSize: 30
+    }
 });
 
 export default HomeScreen;
