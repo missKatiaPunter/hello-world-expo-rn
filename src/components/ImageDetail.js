7f456f1b6234a4cc997c339c86ede4caf3adbfb7
Katia <katia.punter@storyshareplatform.com>
2020-11-14 20:33:11 +0000

    Completes score prop task

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = ({ title }) => {
+const ImageDetail = props => {
+    const { imageSource, title, score } = props;
     return (
         <View>
             <Text style={ styles.textStyle }>
                 { title }
             </Text>
-            <Image source={require('../../assets/beach.jpg')}/>
+            <Image source={ imageSource } style={styles.imgStyle}/>
+            <Text style={styles.textStyle}>Score - { score }</Text>
         </View>
     )
 }
@@ -16,6 +18,9 @@ const styles = StyleSheet.create({
     textStyle: {
         fontSize: 30,
         color: 'purple'
+    },
+    imgStyle:{
+        margin:2
     }
 })
 
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -2,17 +2,30 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import ImageDetail from '../components/ImageDetail';
 
-const ImageScreen = (props) => {
-    console.log(props);
+const ImageScreen = () => {
+
+    const scoreGenerator = num => Math.floor(Math.random() * num) + 1;
+
     return(
         <View>
             <Text style={ styles.textStyle }>
-                This is Images Screen, hello
+                Some Images:
             </Text>
-            <ImageDetail title="Beach"/>
-            <ImageDetail title="Mountain"/>
-            <ImageDetail title="Forest"/>
-            <ImageDetail />
+            <ImageDetail
+                title="Beach"
+                imageSource={ require('../../assets/beach.jpg') }
+                score={ scoreGenerator(10) }
+            />
+            <ImageDetail 
+                title="Mountain" 
+                imageSource={ require('../../assets/mountain.jpg') }
+                score={ scoreGenerator(10) }
+            />
+            <ImageDetail
+                title="Forest"
+                imageSource={ require('../../assets/forest.jpg') }
+                score={ scoreGenerator(10) }
+            />
         </View>
     );
 }
@@ -20,7 +33,7 @@ const ImageScreen = (props) => {
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 40,
-        color: 'red'
+        color: 'red',
     }
 });
 
