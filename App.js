a0a28756e1d2329077864c0de7dc12c19314b9b3
Katia <katia.punter@storyshareplatform.com>
2020-11-17 13:26:05 +0000

    Adds another navigation item to app

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import ImageScreen from './src/screens/ImageScreen';
 import CounterScreen from './src/screens/CounterScreen';
 import ColorScreen from './src/screens/ColorScreen';
 import SquareScreen from './src/screens/SquareScreen';
+import ReducerTrialScreen from './src/screens/ReducerTrialScreen';
 
 const navigator = createStackNavigator(
   {
@@ -19,6 +20,7 @@ const navigator = createStackNavigator(
     Counter: CounterScreen,
     Color: ColorScreen,
     Square: SquareScreen,
+    Reducercol: ReducerTrialScreen,
   },
   {
     initialRouteName: 'Home',
