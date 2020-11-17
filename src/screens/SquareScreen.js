5c5de9e479274b7cd7e28bbbe45128fd688e0441
Katia <katia.punter@storyshareplatform.com>
2020-11-17 12:02:38 +0000

    Completes range checker without reducers

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorDimmer from '../components/ColorDimmer';
 
-const COLOR_INCREMENT = 10;
+const COLOR_INCREMENT = 50;
 
 const SquareScreen = () => {
 
@@ -10,23 +10,39 @@ const SquareScreen = () => {
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
 
+    const setColor = (color, change) => {
+        switch(color){
+            case 'red':
+                (red+change) > 255 || (red+change) < 0 ? null : setRed(red+change);
+                return;
+            case 'green':
+                (green+change) > 255 || (green+change) < 0 ? null : setGreen(green+change);
+                return;
+            case 'blue':
+                (blue+change) > 255 || (blue+change) < 0 ? null : setBlue(blue+change);
+                return;
+            default:
+                return;
+        }
+    }
+
     return <View>
         <Text style={ styles.textStyle }>
             Advanced Color Picker
         </Text>
         <ColorDimmer
-            onIncrease={() => setRed(red+1)}
-            onDecrease={() => setRed(red-1)}
+            onIncrease={() => setColor('red', COLOR_INCREMENT)}
+            onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
             color="red"
         />
         <ColorDimmer
-            onIncrease={() => setGreen(green+1)}
-            onDecrease={() => setGreen(green-1)}
+            onIncrease={() => setColor('green', COLOR_INCREMENT)}
+            onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
             color="green"
         />
         <ColorDimmer
-            onIncrease={() => setBlue(blue+1)}
-            onDecrease={() => setBlue(blue-1)}        
+            onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+            onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}       
             color="blue"
         />
         <View
