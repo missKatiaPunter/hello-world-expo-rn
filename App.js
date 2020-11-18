e31c773b1e09dae1c0adf5f12bd1cc0296de3ece
Katia <katia.punter@storyshareplatform.com>
2020-11-18 13:39:54 +0000

    Adds navigation to new screen

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import CounterScreen from './src/screens/CounterScreen';
 import ColorScreen from './src/screens/ColorScreen';
 import SquareScreen from './src/screens/SquareScreen';
 import ReducerTrialScreen from './src/screens/ReducerTrialScreen';
+import CounterWithReducer from './src/screens/CounterWithReducer';
 
 const navigator = createStackNavigator(
   {
@@ -21,6 +22,7 @@ const navigator = createStackNavigator(
     Color: ColorScreen,
     Square: SquareScreen,
     Reducercol: ReducerTrialScreen,
+    Reducercounter: CounterWithReducer,
   },
   {
     initialRouteName: 'Home',
