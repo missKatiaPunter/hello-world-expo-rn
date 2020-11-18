a5839ffd4081ebef35c327963a82433cad497e07
Katia <katia.punter@storyshareplatform.com>
2020-11-18 07:50:33 +0000

    Starts the reducer

---
+++ src/screens/ReducerTrialScreen.js
@@ -0,0 +1,26 @@
+import React, { useReducer } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import ColorDimmer from '../components/ColorDimmer';
+
+const COLOR_INCREMENT = 25;
+
+const reducer = (state, action) => {
+    
+}
+
+const ReducerTrialScreen = () => {
+
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0})
+
+    return <View>
+        <Text>Pick Color with Reducer</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    containerStyle:{
+        padding: 20,
+    }
+});
+
+export default ReducerTrialScreen;
